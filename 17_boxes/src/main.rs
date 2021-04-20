6793f900e92f197b64202e8862ae91493c36bbbe
oreqizer <oreqizer@gmail.com>
2021-04-20 08:48:35 +0200

    smart pointers

--- 17_boxes/src/main.rs
+++ 17_boxes/src/main.rs
@@ -1,3 +1,15 @@
+use std::ops::Deref;
+
+fn main() {
+    // Box is essentially a "fat pointer" AFAIK
+    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+
+    // Deref coersion
+    let m = MyBox::new(String::from("Rust"));
+    hello(&m);
+    // -> hello(&(*m)[..]);
+}
+
 enum List {
     Cons(i32, Box<List>),
     Nil,
@@ -5,7 +17,24 @@ enum List {
 
 use crate::List::{Cons, Nil};
 
-fn main() {
-    // Box is essentially a "fat pointer" AFAIK
-    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+// Enables dereference operator "*"
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        &self.0
+    }
+}
+
+// Fust a silly function
+fn hello(name: &str) {
+    println!("Hello, {}!", name);
 }
